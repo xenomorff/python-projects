2457f42f854d05bf365f7d7121e6dc1656e09669
xenomorff <brandonjenks32400@gmail.com>
2014-12-16 11:57:35 -0500

    math tests

---
+++ multiply.py
@@ -0,0 +1,10 @@
+name = input('What is your name?')
+print('Hello '+name)
+
+first = int(input('First number please:'))
+
+second = int(input('second number please:'))
+
+
+total = (first*second)
+print(total)
---
+++ testmath.py
@@ -0,0 +1,10 @@
+name = input('what is your name?')
+print('hello ' +name)
+
+first = int(input('please enter first number:'))
+
+second = int(input('second number:'))
+             
+total = first+second
+print('the answer is {},{}'.format(total,name))
+

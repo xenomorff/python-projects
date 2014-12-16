3fd4ad923d8fdc5b4e2f2d3e1cfe198cbaeef40c
Brandon Jenks <brandonjenks32400@gmail.com>
2014-12-16 14:41:30 -0600

    divison calculator

---
+++ divison calculator.py
@@ -0,0 +1,13 @@
+name = input('what is your name?')
+
+print('Hello ' +name)
+print('Welcome to divison calculator')
+
+one = int(input('What is the first number of the problem?:'))
+
+two = int (input('what is the second number of the problem?:'))
+
+total = one / two
+
+print('The answer is {},{},' .format(total,name))
+

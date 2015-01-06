4f3f3f3ba1a806ffa93d37f55a0d3029a2855705
xenomorff <brandonjenks32400@gmail.com>
2015-01-06 11:00:45 -0500

    idk

---
+++ idk.py
@@ -0,0 +1,12 @@
+import time
+
+countdown = 5
+
+while countdown > 0:
+    print('Your dead in..')
+    print(countdown)
+    countdown = countdown - 1
+    time.sleep(1)
+print('your dead')
+    
+    
